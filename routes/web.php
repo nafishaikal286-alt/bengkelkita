ab7d5ea5d2e9e1d1f5152add24404c3a24d6bfd3
nafishaikal286-alt <nafishaikal286@gmail.com>
2025-12-13 19:19:03 +0700

    Simpan perubahan lokal

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,10 @@ Route::get('/login', function () {
     return view('bengkelkita.login');
 });
 
+Route::middleware(['auth','is_admin'])->group(function () {
+    Route::get('/admin/dashboard', [AdminDashboardController::class, 'index']);
+});
+
 Route::get('/', fn () => view('bengkelkita.splash'));
 
 Route::get('/register', fn () => view('bengkelkita.register'));
